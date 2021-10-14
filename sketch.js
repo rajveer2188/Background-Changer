3379a939139a7caa1d1dc0fad85cdf3a413d1487
rajveer2188 <92036661+rajveer2188@users.noreply.github.com>
2021-10-14 21:09:12 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+var s1
+var sprite = []
+function setup() 
+{
+  createCanvas(400,400);
+s1 = createSprite(200,200,10,10)
+sprite[0] = s1
+
+console.log(sprite[0].x)
+
+}
+
+function draw() 
+{
+background(51);
+
+}
+
